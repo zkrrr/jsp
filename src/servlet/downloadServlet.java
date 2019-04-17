c5a67b4f4962713ef156c200705e1710453dbfa6
zkrrr <zk4work@163.com>
2019-04-17 16:25:59 +0800

    download

---
+++ src/servlet/downloadServlet.java
@@ -0,0 +1,65 @@
+package servlet;
+/*
+ * create by zk
+ * 19-4-17
+ */
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@WebServlet("/download")
+public class downloadServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        try {
+            request.setCharacterEncoding("UTF-8");
+        } catch (UnsupportedEncodingException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        java.io.BufferedInputStream bis = null;
+        java.io.BufferedOutputStream bos = null;
+
+        String downLoadPath = request.getServletContext().getRealPath("/subjectImage") + "/timg(13).jpeg";  //注意不同系统的分隔符
+        System.out.println(downLoadPath);
+        String fileName = "downloadFile";
+
+        try {
+            long fileLength = new File(downLoadPath).length();
+            response.setContentType("application/x-msdownload;");
+            response.setHeader("Content-disposition", "attachment; filename=" + new String(fileName.getBytes("utf-8"), "ISO8859-1"));
+            response.setHeader("Content-Length", String.valueOf(fileLength));
+            bis = new BufferedInputStream(new FileInputStream(downLoadPath));
+            bos = new BufferedOutputStream(response.getOutputStream());
+            byte[] buff = new byte[2048];
+            int bytesRead;
+            while (-1 != (bytesRead = bis.read(buff, 0, buff.length))) {
+                bos.write(buff, 0, bytesRead);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null)
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            if (bos != null)
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+        }
+    }
+}
---
+++ web/download.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zk
+  Date: 19-4-17
+  Time: 下午4:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form action="download" method="post">
+        <input type="submit" value="下载">
+    </form>
+</body>
+</html>
